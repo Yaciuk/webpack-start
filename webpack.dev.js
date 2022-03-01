4d067cfddb69314501c1c09e8a40b87210beac85
Yaciuk <yaciuknicole@gmail.com>
2022-02-28 22:23:17 -0600

    not sure why I'm having issues loading assets/*.svg

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const common = require("./webpack.common");
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require("webpack-merge");
 
-module.exports = merge(common, {
+module.exports = merge (common, {
   mode: "development",
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),

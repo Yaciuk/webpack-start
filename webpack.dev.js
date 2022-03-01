b68615eda80745f6eb40056e4084872e4e2058b6
Yaciuk <yaciuknicole@gmail.com>
2022-02-28 21:12:40 -0600

    added npm start for dev and npm build (npm run build) in package.json, updated webpack.dev, webpack.prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,11 +3,11 @@ const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true
   }
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,12 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = merge (common, {
   mode: "production",
-  devtool: false,
   output: {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true
   }
-};
+});
